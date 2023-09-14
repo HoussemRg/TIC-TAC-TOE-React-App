144976be3c7dc81e701bf7cf904faf9406a40637
houssem <houssem.regaieg@etudiant-enit.utm.tn>
2023-09-14 01:33:21 +0100

    still to work on the function of displaying results of game

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,179 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState,useCallback } from 'react';
 import './App.css';
 import GameBody from './Components/gameBody/gameBody';
 import Header from "./Components/header/header";
 
 export interface IUserTurn{
-  xTurn:boolean
-  oTurn:boolean
- 
+  xturn:boolean
+  oturn:boolean
+  buttonId:number
+  isClicked:boolean
 }
+interface IPlayerClicks{
+  xClicks:number[]
+  oClicks:number[]
+}
+export interface IResults{
+  xWinner:number
+  oWinner:number
+  ties:number
+}
+const arrayOfButtons:IUserTurn[] =[
+  {
+    xturn:false,
+    oturn:false,
+    buttonId:1,
+    isClicked:false,
+  },
+  {
+    xturn:false,
+    oturn:false,
+    buttonId:2,
+    isClicked:false,
+  },
+  {
+    xturn:false,
+    oturn:false,
+    buttonId:3,
+    isClicked:false,
+  },
+  {
+    xturn:false,
+    oturn:false,
+    buttonId:4,
+    isClicked:false,
+  },
+  {
+    xturn:false,
+    oturn:false,
+    buttonId:5,
+    isClicked:false,
+  },
+  {
+    xturn:false,
+    oturn:false,
+    buttonId:6,
+    isClicked:false,
+  },
+  {
+    xturn:false,
+    oturn:false,
+    buttonId:7,
+    isClicked:false,
+  },
+  {
+    xturn:false,
+    oturn:false,
+    buttonId:8,
+    isClicked:false,
+  },
+  {
+    xturn:false,
+    oturn:false,
+    buttonId:9,
+    isClicked:false,
+  }
+];
+
 
 function App() {
-  const [turn,setTurn]=useState<IUserTurn>({xTurn:true,oTurn:false});
-  const playTurn=():void=>{
-    const newTurn:IUserTurn={
-      xTurn:false,
-      oTurn:true,
-    }
-    setTurn(newTurn);
+  const [turn,setTurn]=useState<IUserTurn[]>(arrayOfButtons);
+  const [clicks,setClicks]=useState<IPlayerClicks>({xClicks:[],oClicks:[]});
+  const [results,setResults]=useState<IResults>({xWinner:0,ties:0,oWinner:0})
+  const playTurn=(id:number):void=>{
+    const newArray:IUserTurn[]=turn.map((button:IUserTurn)=>{
+      if(button.buttonId===id){
+        return({
+          buttonId:button.buttonId,
+          isClicked:true,
+          xturn:turn.filter((button)=>button.isClicked).length%2===0,
+          oturn:turn.filter((button)=>button.isClicked).length%2!==0,
+        });
+      }else {
+        return button; 
+      }
+      
+  });
+    const newXTurnsArray:number[]=newArray.filter((button)=>button.isClicked && button.xturn).map((button)=>{
+      return(button.buttonId);
+    });
+    const newOTurnsArray:number[]=newArray.filter((button)=>button.isClicked && button.oturn).map((button)=>{
+      return(button.buttonId);
+    });
+    const newClicks:IPlayerClicks={xClicks:newXTurnsArray,oClicks:newOTurnsArray};
+    setTurn(newArray);
+    setClicks(newClicks);
+    
+    
   }
+  
+  const declareWinner=():void=>{
+    const winningCombinations = [
+      [1, 2, 3], // Première ligne horizontale
+      [4, 5, 6], // Deuxième ligne horizontale
+      [7, 8, 9], // Troisième ligne horizontale
+      [1, 4, 7], // Première ligne verticale
+      [2, 5, 8], // Deuxième ligne verticale
+      [3, 6, 9], // Troisième ligne verticale
+      [1, 5, 9], // Diagonale de gauche à droite
+      [3, 5, 7], // Diagonale de droite à gauche
+    ];
+    
+    winningCombinations.forEach((comb)=>{
+      if(comb.every((num)=> clicks.xClicks.includes(num))){
+        const newXwinner=results.xWinner++;
+        const oldTie=results.ties;
+        const oldoWinner=results.oWinner
+        setResults({xWinner:newXwinner,ties:oldTie,oWinner:oldoWinner});
+        console.log(results);
+      }
+      else if(comb.every((num)=> clicks.oClicks.includes(num))){
+        const newOwinner=results.oWinner++;
+        const oldTie=results.ties;
+        const oldXWinner=results.xWinner
+        setResults({xWinner:oldXWinner,ties:oldTie,oWinner:newOwinner});
+        console.log(results);
+      }
+    })
+    if(clicks.xClicks.length===5){
+      const newTie=results.ties++;
+      setResults({xWinner:results.xWinner,ties:newTie,oWinner:results.oWinner});
+      
+    }
+  }
+
+  useEffect(()=>{
+    declareWinner();
+  },[clicks])
+  
+  
   return (
     
     <div className="App">
       <Header />
-      <GameBody turn={turn} playTurn={playTurn} />
-      
-      
+      <GameBody turn={turn} playTurn={playTurn} results={results} />
+      <div className="footer-grid">
+        <div className="results">
+          <div className="x">
+                <div>X</div>
+                <div>{results.xWinner}</div>
+            </div>
+            <div className="ties">
+                <div>Ties</div>
+                <div>{results.ties}</div>    
+            </div>
+            <div className="o">
+                <div>O</div>
+                <div>{results.oWinner}</div>    
+            </div>
+            <div className="winner">
+                WINS
+            </div>
+        </div>
+      </div>
     </div>
+      
+    
   );
 }
 export default App;
--- src/Components/gameBody/gameBody.tsx
+++ src/Components/gameBody/gameBody.tsx
@@ -1,49 +1,34 @@
 import './gameBody.css';
-import { IUserTurn } from '../../App';
+import { IResults, IUserTurn } from '../../App';
 
 interface IPlayTurn{
-    turn:IUserTurn
-    playTurn:()=>void
+    turn:IUserTurn[]
+    playTurn:(id:number)=>void
+    results:IResults
 }
 
 
 
 
 function GameBody(props:IPlayTurn) {
+   
+    const handleClick=(id:number):void=>{
+        props.playTurn(id);
+    }
+    
     
   return (
     <div className="gameBody">
         <div className="grid">
-        <button className="box" onClick={props.playTurn}>{ props.turn.xTurn && <span className="material-symbols-outlined xBodySymbole">close</span>}{ props.turn.oTurn && <span className="material-symbols-outlined oBodySymbole">trip_origin</span>}</button>
-        <button className="box" onClick={props.playTurn}>{ props.turn.xTurn && <span className="material-symbols-outlined xBodySymbole">close</span>}{ props.turn.oTurn && <span className="material-symbols-outlined oBodySymbole">trip_origin</span>}</button>
-        <button className="box" onClick={props.playTurn}>{ props.turn.xTurn && <span className="material-symbols-outlined xBodySymbole">close</span>}{ props.turn.oTurn && <span className="material-symbols-outlined oBodySymbole">trip_origin</span>}</button>
-        <button className="box" onClick={props.playTurn}>{ props.turn.xTurn && <span className="material-symbols-outlined xBodySymbole">close</span>}{ props.turn.oTurn && <span className="material-symbols-outlined oBodySymbole">trip_origin</span>}</button>
-        <button className="box" onClick={props.playTurn}>{ props.turn.xTurn && <span className="material-symbols-outlined xBodySymbole">close</span>}{ props.turn.oTurn && <span className="material-symbols-outlined oBodySymbole">trip_origin</span>}</button>
-        <button className="box" onClick={props.playTurn}>{ props.turn.xTurn && <span className="material-symbols-outlined xBodySymbole">close</span>}{ props.turn.oTurn && <span className="material-symbols-outlined oBodySymbole">trip_origin</span>}</button>
-        <button className="box" onClick={props.playTurn}>{ props.turn.xTurn && <span className="material-symbols-outlined xBodySymbole">close</span>}{ props.turn.oTurn && <span className="material-symbols-outlined oBodySymbole">trip_origin</span>}</button>
-        <button className="box" onClick={props.playTurn}>{ props.turn.xTurn && <span className="material-symbols-outlined xBodySymbole">close</span>}{ props.turn.oTurn && <span className="material-symbols-outlined oBodySymbole">trip_origin</span>}</button>
-        <button className="box" onClick={props.playTurn}>{ props.turn.xTurn && <span className="material-symbols-outlined xBodySymbole">close</span>}{ props.turn.oTurn && <span className="material-symbols-outlined oBodySymbole">trip_origin</span>}</button>
-        <button className="x">
-            <div>
-                <div>X</div>
-                <div>14</div>
-            </div>
-        </button>
-        <button className="ties">
-            <div>
-                <div>Ties</div>
-                <div>14</div>
-            </div>
-        </button>
-        <button className="o">
-            <div>
-                <div>O</div>
-                <div>14</div>
-            </div>
-        </button>
-        <button className="winner">
-            WINS
-        </button>
+        <button className="box" onClick={()=>handleClick(1)} disabled={props.turn[0].isClicked}>{props.turn[0].isClicked  && props.turn[0].xturn && <span className="material-symbols-outlined xBodySymbole">close</span>} {props.turn[0].isClicked  && props.turn[0].oturn && <span className="material-symbols-outlined oBodySymbole">trip_origin</span>}</button>
+        <button className="box" onClick={()=>handleClick(2)} disabled={props.turn[1].isClicked}>{props.turn[1].isClicked  && props.turn[1].xturn && <span className="material-symbols-outlined xBodySymbole">close</span>} {props.turn[1].isClicked  && props.turn[1].oturn && <span className="material-symbols-outlined oBodySymbole">trip_origin</span>}</button>
+        <button className="box" onClick={()=>handleClick(3)} disabled={props.turn[2].isClicked}>{props.turn[2].isClicked  && props.turn[2].xturn && <span className="material-symbols-outlined xBodySymbole">close</span>} {props.turn[2].isClicked  && props.turn[2].oturn && <span className="material-symbols-outlined oBodySymbole">trip_origin</span>}</button>
+        <button className="box" onClick={()=>handleClick(4)} disabled={props.turn[3].isClicked}>{props.turn[3].isClicked  && props.turn[3].xturn && <span className="material-symbols-outlined xBodySymbole">close</span>} {props.turn[3].isClicked  && props.turn[3].oturn && <span className="material-symbols-outlined oBodySymbole">trip_origin</span>}</button>
+        <button className="box" onClick={()=>handleClick(5)} disabled={props.turn[4].isClicked}>{props.turn[4].isClicked  && props.turn[4].xturn && <span className="material-symbols-outlined xBodySymbole">close</span>} {props.turn[4].isClicked  && props.turn[4].oturn && <span className="material-symbols-outlined oBodySymbole">trip_origin</span>}</button>
+        <button className="box" onClick={()=>handleClick(6)} disabled={props.turn[5].isClicked}>{props.turn[5].isClicked  && props.turn[5].xturn && <span className="material-symbols-outlined xBodySymbole">close</span>} {props.turn[5].isClicked  && props.turn[5].oturn && <span className="material-symbols-outlined oBodySymbole">trip_origin</span>}</button>
+        <button className="box" onClick={()=>handleClick(7)} disabled={props.turn[6].isClicked}>{props.turn[6].isClicked  && props.turn[6].xturn && <span className="material-symbols-outlined xBodySymbole">close</span>} {props.turn[6].isClicked  && props.turn[6].oturn && <span className="material-symbols-outlined oBodySymbole">trip_origin</span>}</button>
+        <button className="box" onClick={()=>handleClick(8)} disabled={props.turn[7].isClicked}>{props.turn[7].isClicked  && props.turn[7].xturn && <span className="material-symbols-outlined xBodySymbole">close</span>} {props.turn[7].isClicked  && props.turn[7].oturn && <span className="material-symbols-outlined oBodySymbole">trip_origin</span>}</button>
+        <button className="box" onClick={()=>handleClick(9)} disabled={props.turn[8].isClicked}>{props.turn[8].isClicked  && props.turn[8].xturn && <span className="material-symbols-outlined xBodySymbole">close</span>} {props.turn[8].isClicked  && props.turn[8].oturn && <span className="material-symbols-outlined oBodySymbole">trip_origin</span>}</button>
         </div>
     </div>
   );
